7afbea2c37acf28eed0908bef2c70aed6ca2ec19
Oleksii_Dorofieiev <oleksii.dorofieiev@kingfisher.com>
2024-07-29 10:24:52 +0300

    added tests

--- src/test/java/tests/api/TestsPet.java
+++ src/test/java/tests/api/TestsPet.java
@@ -14,12 +14,15 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import epam.api.dto.Pet;
 import epam.api.utils.RestUtilities;
+import static epam.data.StatusCode.NOT_FOUND_404;
 import static epam.data.StatusCode.OK_200;
 import static epam.data.StringConstants.CONSTANT_VALUE_INT;
 import static epam.data.StringConstants.ID;
 import static epam.data.StringConstants.MESSAGE;
+import static epam.data.StringConstants.NAME;
 import static epam.data.StringConstants.PET_ID;
 import static epam.data.StringConstants.PET_NOT_FOUND;
+import static epam.data.StringConstants.STATUS;
 import epam.data.TestData;
 import static io.restassured.RestAssured.given;
 import io.restassured.response.Response;
@@ -81,43 +84,38 @@ public class TestsPet {
 
     @Test
     public void getPetsByExistingId() {
-        int existingId = 1; // Assuming 1 is an existing ID
-
-        given()
-                .spec(requestSpec)
-                .when()
-                .get(PET_ID, existingId)
-                .then()
-                .statusCode(OK_200)
-                .and()
-                .body(ID, equalTo(existingId));
+        given().spec(requestSpec)
+               .header("api_key", "{{apiKey}}")
+               .pathParam("petId", petDog.getId())
+               .when().get(PET_ID)
+               .then()
+               .statusCode(OK_200)
+               .and()
+               .body(ID, equalTo(petDog.getId()))
+               .body(NAME, equalTo(petDog.getName()))
+               .body(STATUS, equalTo("available"));
     }
-
+    
     @Test
     public void getPetsByNotExistingId() {
-        int notExistingId = CONSTANT_VALUE_INT; // Assuming CONSTANT_VALUE_INT is a non-existing ID
-
-        given()
-                .spec(requestSpec)
-                .when()
-                .get(PET_ID, notExistingId)
-                .then()
-                .statusCode(OK_200)
-                .and()
-                .body(MESSAGE, equalTo(PET_NOT_FOUND));
+        given().spec(requestSpec)
+               .header("api_key", "{{apiKey}}")
+               .pathParam("petId", notExistingId)
+               .when().get(PET_ID)
+               .then()
+               .statusCode(NOT_FOUND_404);
     }
-
+    
     @Test
     public void testFindPetById_NotFound() {
-
-        given()
-                .spec(requestSpec)
-                .when()
-                .get(PET_ID, notExistingId)
-                .then()
-                .statusCode(OK_200)
-                .and()
-                .body(MESSAGE, equalTo(PET_NOT_FOUND));
+        given().spec(requestSpec)
+               .header("api_key", "{{apiKey}}")
+               .pathParam("petId", notExistingId)
+               .when().get(PET_ID)
+               .then()
+               .statusCode(NOT_FOUND_404)
+               .and()
+               .body(MESSAGE, equalTo(PET_NOT_FOUND));
     }
 
     @AfterAll
